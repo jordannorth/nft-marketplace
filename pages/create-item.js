76d7d81339afc941483296027762cc43046f2bb4
Jordan North <jordannorth1@hotmail.co.uk>
2022-05-21 16:09:58 +0100

    Create digital asset

--- pages/create-item.js
+++ pages/create-item.js
@@ -77,7 +77,58 @@ export default function CreateItem() {
         let transaction = await contract.createToken(url)
         let tx = await transaction.wait()
 
+        let event = tx.events[0]
+        let value = event.args[2]
+        let tokenId = value.toNumber()
+
+        const price = ethers.utils.pareseUnits(formInput.price, 'ether')
+        contract = new ethers.Contract(nftMarketAddress, MarketPlace.abi, signer)
+        let listingPrice = await contract.getListingPrice()
+        listingPrice = listingPrice.toString()
+
+        transaction = await contract.createMarketItem(nftAddress, tokenId, price, { value: listingPrice })
+        await transaction.wait()
+
+        // Send back to main page 
+        router.push('/')
 
     }
 
+    return (
+        <div className='flex justify-center'>
+            {/* Creating a form to allow users to specify the asset */}
+            <div className='w-1/2 flex flex-col pb-12'>
+                <input
+                    placeholder='Asset Name'
+                    className='mt-8 border rounded p-4'
+                    // Will return the contents of the form and update only the name
+                    onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
+                />
+                <textarea
+                    placeholder='Asset Description'
+                    className='mt-2 border rounded p-4'
+                    onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
+                />
+                <input
+                    placeholder='Asset Price in Matic'
+                    className='mt-2 border rounded p-4'
+                    // Will return the contents of the form and update only the name
+                    onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
+                />
+                <input
+                    type='file'
+                    name='Asset'
+                    className='my-4'
+                    onChange={onChange}
+                />
+                {
+                    fileUrl && (<img className='rounded mt-4' width='350' src={fileUrl}/>)
+                }
+                <button onClick={createItem} className='font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg'>
+                    Create Digital Asset
+                </button>
+            </div>
+        </div>
+
+    )
 }
