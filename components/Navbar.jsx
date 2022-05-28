bf2e0f4cedc3343e96614064bb57bc78045a080f
Jordan North <jordannorth1@hotmail.co.uk>
2022-05-28 15:31:27 +0100

    WIP styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,34 +2,43 @@ import React from "react";
 // Used for lazy loading images
 import Image from "next/image";
 import Link from "next/link";
+import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai"
 
 const Navbar = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-      <Image
-        src="/../public/assets/duck-logo.png"
-        alt="/"
-        width="125"
-        height="100"
-      />
-      <div>
-          <ul>
-             <Link href='/create-item'>
-                 <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
-             </Link>
-             <Link href='/'>
-                 <li className="ml-10 text-sm uppercase hover:border-b">Sell</li>
-             </Link>
-             <Link href='/'>
-                 <li className="ml-10 text-sm uppercase hover:border-b">My NFT's</li>
-             </Link>
-             <Link href='/'>
-                 <li className="ml-10 text-sm uppercase hover:border-b">Creator</li>
-             </Link>
+        <Image
+          src="/../public/assets/duck-logo.png"
+          alt="/"
+          width="125"
+          height="100"
+        />
+        <div>
+          {/* Hidden, anything above medium we display as flex */}
+          <ul className="hidden md:flex">
+            <Link href="/create-item">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Sell</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                My NFT's
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Creator
+              </li>
+            </Link>
           </ul>
-
-      </div>
+          <div className='flex md:hidden'>
+              <AiOutlineMenu size={25}/>
+             
+              </div>
+        </div>
       </div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -8,29 +8,10 @@ function MyApp({ Component, pageProps }) {
     <div>
       <Head>
         <title>NFT Marketplace</title>
-        {/* <meta name="Description" content="NFT marketplace by JN" />
-        <link rel="icon" src="/..public/assets/duck-logo.png" /> */}
+        <meta name="Description" content="NFT marketplace by JN" />
+        {/* <link rel="icon" src="/..public/assets/duck-logo.png" />  */}
       </Head>
       <Navbar />
-
-      <nav className="border-b p-6">
-        <img src="../img/duck-logo.png" />
-        <p className="text-4xl font-bold">Metaverse Marketplace</p>
-        <div className="flex mt-4">
-          <Link href="/">
-            <a className="mr-4 text-pink-500">Home</a>
-          </Link>
-          <Link href="/create-item">
-            <a className="mr-6 text-pink-500">Sell Digital Asset</a>
-          </Link>
-          <Link href="/my-assets">
-            <a className="mr-6 text-pink-500">My Digital Assets</a>
-          </Link>
-          <Link href="/my-assets">
-            <a className="mr-6 text-pink-500">Creator Dashboard</a>
-          </Link>
-        </div>
-      </nav>
       <Component {...pageProps} />
     </div>
   );
